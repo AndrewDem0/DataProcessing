8947ac7eb362b8e060fcbf42ef8dbf75f81c9181
AndrewDem0 <144722259+AndrewDem0@users.noreply.github.com>
2024-05-12 19:56:58 +0300

    lab 4 done
    Servlet1 updated
    SqlCRUD updated

--- lab1 folder/lab1/src/main/java/jdbc/SqlCRUD.java
+++ lab1 folder/lab1/src/main/java/jdbc/SqlCRUD.java
@@ -7,8 +7,7 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SqlCRUD implements LabCRUDInterface<User> {
-
+public class SqlCRUD implements LabCRUDInterface <User> {
     Connection connection;
 
     public SqlCRUD() {
@@ -26,15 +25,15 @@ public class SqlCRUD implements LabCRUDInterface<User> {
 
     @Override
     public void create(User user) {
-        try( PreparedStatement st=
-                connection.prepareStatement("INSERT INTO entity(name,age)" + "VALUES(?,?) ")){
+        try(PreparedStatement st =
+                    connection.prepareStatement("INSERT INTO entity (name, age) " +
+                            "VALUES (?,?)");){
             st.setString(1, user.getName());
             st.setInt(2, user.getAge());
             st.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
     }
 
     @Override
@@ -43,15 +42,11 @@ public class SqlCRUD implements LabCRUDInterface<User> {
 
         try(
                 Statement st = connection.createStatement();
-                ResultSet rs= st.executeQuery("SELECT * FROM entity;");
-
-                )
-        {
+                ResultSet rs = st.executeQuery("SELECT * FROM entity;");
+        ) {
             while (rs.next()){
-                list.add(new User(rs.getInt(1), rs.getString(2), rs.getInt(3)));
-
+                list.add(new User(rs.getInt(1),rs.getString(2),rs.getInt(3)));
             }
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -60,8 +55,8 @@ public class SqlCRUD implements LabCRUDInterface<User> {
 
     @Override
     public void update(int id, User user) {
-        try( PreparedStatement st=
-                     connection.prepareStatement("UPDATE INTO entity(name,age)" + "SET\"name\"=?, \"age\"=? WHERE id=?;" )){
+        try(PreparedStatement st =
+                    connection.prepareStatement("UPDATE entity SET \"name\"=?, \"age\"=? WHERE id=?;");){
             st.setString(1, user.getName());
             st.setInt(2, user.getAge());
             st.setInt(3, user.getId());
@@ -69,18 +64,16 @@ public class SqlCRUD implements LabCRUDInterface<User> {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
     }
 
     @Override
     public void delete(int id) {
-        try( PreparedStatement st=
-                     connection.prepareStatement("DELETE FROM entity WHERE id?" )){
+        try(PreparedStatement st =
+                    connection.prepareStatement("DELETE FROM entity WHERE id=?;");){
             st.setInt(1, id);
             st.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
     }
 }
